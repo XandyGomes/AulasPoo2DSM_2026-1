38f84840128d6e701f8f3191c2b91651187ddfd9
XandyGomes <x3.computadores@hotmail.com>
2026-02-27 19:25:09 -0300

    Aula.23.02.2026

---
+++ src/main/java/com/alexandre/aulaspoo2dsm/Soma.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.alexandre.aulaspoo2dsm;
+
+/**
+ *
+ * @author Alexandre
+ */
+public class Soma {
+    public static void main(String[] args) {
+        int number1, number2, result;
+        
+        number1 = 20;
+        number2 = 30;
+        
+        result = number1 + number2;
+        
+        
+        System.out.print("A soma é "+ result);
+        
+        
+    }
+}
