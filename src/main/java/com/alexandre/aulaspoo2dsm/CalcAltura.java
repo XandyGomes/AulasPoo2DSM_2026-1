64b48be45b940a0a8f33918272b3da77b602d3b5
XandyGomes <x3.computadores@hotmail.com>
2026-03-09 21:05:01 -0300

    Add CalcAltura class with user input functionality and update Impressao class for output demonstration

--- src/main/java/com/alexandre/aulaspoo2dsm/CalcAltura.java
+++ src/main/java/com/alexandre/aulaspoo2dsm/CalcAltura.java
@@ -8,17 +8,11 @@ package com.alexandre.aulaspoo2dsm;
  *
  * @author Alexandre
  */
-public class Impressao {
+public class CalcAltura {
+
     public static void main(String[] args) {
-        
-        System.out.println("1, 2, 3, 4");
-        
-        System.out.print("1, ");
-        System.out.print("2, ");
-        System.out.print("3, ");
-        System.out.print("4");
-        
-        System.out.printf("%n%d, %d, %d, %d", 1, 2, 3, 4);
-                
+     for (int i = 1; i <= 5; i++) {
+            System.out.println("Digite o "+ i +"º número: ");
+        }
     }
 }
